5173cd7f802b04e7ec9ef376564c05f157e8a89c
MatthewolVGD <1707240@mukilteo.wednet.edu>
2021-03-17 15:20:06 -0700

    Have a moving, bouncing, destructable ball

---
+++ Assets/Matthew/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    #region Main Variables
+    Rigidbody2D rb2;
+    public float initSpeed; //Speed ball starts at
+    public float addPercentage; //Percentage of current speed added whenever ball hits something, 1.1 means 10% added
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = gameObject.GetComponent<Rigidbody2D>();
+
+        rb2.velocity = new Vector2(initSpeed, initSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Hit something");
+        if (collision.gameObject.tag == "Wall")
+        {
+            rb2.velocity = Vector3.Reflect(rb2.velocity, Vector3.up);
+            rb2.velocity = rb2.velocity * addPercentage;
+        }
+        if (collision.gameObject.tag == "Paddle")
+        {
+            rb2.velocity = Vector3.Reflect(rb2.velocity, collision.gameObject.transform.right);
+            rb2.velocity = rb2.velocity * addPercentage;
+        }
+        if (collision.gameObject.tag == "Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
