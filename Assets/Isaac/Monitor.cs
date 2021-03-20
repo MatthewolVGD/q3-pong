eeec0262d5b8e70fb61891982c2fde19d365a567
DESKTOP-M97TLDN\isaac <ikemikehall@gmail.com>
2021-03-19 17:04:32 -0700

    lots of menu shit

---
+++ Assets/Isaac/Monitor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Monitor : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            SceneManager.LoadScene("CreditRoller"); //Requires "Using" (see above)
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            SceneManager.LoadScene("MainLevel1"); //Requires "Using" (see above)
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+
+            SceneManager.LoadScene("MainMenu"); //Requires "Using" (see above)
+
+        }
+
+
+
+
+    }
+}
