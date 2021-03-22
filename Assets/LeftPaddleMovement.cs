9ad2e82c4504cfc8bd24ac6cb9370227715955da
DESKTOP-037SL6H\Owner <Baconlad123@gmail.com>
2021-03-22 15:03:08 -0700

    Absolute big brain paddle plays over here

---
+++ Assets/LeftPaddleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPaddleMovement : MonoBehaviour
+{
+    public float scaler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float paddleMove = Input.GetAxis("LeftP");
+        transform.position += new Vector3(0, paddleMove, 0) * scaler;
+        float yPos = Mathf.Clamp(transform.position.y, -5.0f, 5.0f);
+        transform.position = new Vector3(transform.position.x, yPos, 0);
+
+    }
+}
---
+++ Assets/RightPaddleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPaddleMovement : MonoBehaviour
+{
+    public float scaler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float paddleMove = Input.GetAxis("RightP");
+        transform.position += new Vector3(0, paddleMove, 0) * scaler;
+        float yPos = Mathf.Clamp(transform.position.y, -5.0f, 5.0f);
+        transform.position = new Vector3(transform.position.x, yPos, 0);
+
+    }
+}
