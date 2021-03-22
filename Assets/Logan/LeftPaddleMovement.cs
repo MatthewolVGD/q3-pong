31d713d005497d28cd308001325bc706edb1a3ce
DESKTOP-037SL6H\Owner <Baconlad123@gmail.com>
2021-03-22 15:40:13 -0700

    Bigger brain paddle plays (rotation)

--- Assets/Logan/LeftPaddleMovement.cs
+++ Assets/Logan/LeftPaddleMovement.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightPaddleMovement : MonoBehaviour
+public class LeftPaddleMovement : MonoBehaviour
 {
     public float scaler;
+    public float scaler2;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,20 @@ public class RightPaddleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float paddleMove = Input.GetAxis("RightP");
+        float paddleMove = Input.GetAxis("LeftP");
+        float paddleRotate = Input.GetAxis("LeftPR");
         transform.position += new Vector3(0, paddleMove, 0) * scaler;
         float yPos = Mathf.Clamp(transform.position.y, -5.0f, 5.0f);
         transform.position = new Vector3(transform.position.x, yPos, 0);
 
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * scaler2);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * scaler2);
+        }
     }
 }
--- Assets/Logan/RightPaddleMovement.cs
+++ Assets/Logan/RightPaddleMovement.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeftPaddleMovement : MonoBehaviour
+public class RightPaddleMovement : MonoBehaviour
 {
     public float scaler;
+    public float scaler2;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,20 @@ public class LeftPaddleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float paddleMove = Input.GetAxis("LeftP");
+        float paddleMove = Input.GetAxis("RightP");
+        float paddleRotate = Input.GetAxis("RightPR");
         transform.position += new Vector3(0, paddleMove, 0) * scaler;
         float yPos = Mathf.Clamp(transform.position.y, -5.0f, 5.0f);
         transform.position = new Vector3(transform.position.x, yPos, 0);
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * scaler2);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * scaler2);
+        }
     }
 }
